6e2770b60e9fcf505fb9a8bd579734f2c37e1e9c
molluk <molluk@mail.ru>
2021-03-21 14:43:10 +0300

    21.03.2021 14:42

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,12 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld helloWorldBean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld helloWorldBean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println("helloWorld = " + helloWorldBean1.equals(helloWorldBean2));
+        Cat catBean1 = (Cat) applicationContext.getBean("cat");
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+        System.out.println("Cat = " + catBean1.equals(catBean2));
+
     }
 }
